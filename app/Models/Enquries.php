6f526cff61032861ce63f800111de16ea5d9a5eb
pradipta-pd <pradipta.bhuin@unifiedinfotech.net>
2024-10-25 11:11:57 +0530

    time zone

--- app/Models/Enquries.php
+++ app/Models/Enquries.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Enquries extends Model
 {
     use HasFactory;
+    public function getCreatedAtAttribute($value)
+    {
+        // return Carbon::parse($value)->setTimezone(config('app.timezone'))->toDateTimeString();
+        return Carbon::parse($value)->timezone('Asia/Kolkata')->toDateTimeString();
+    }
 }
