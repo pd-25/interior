6b981402c5b875da6d91949f56c62edea721a66e
pradipta-pd <pradipta.bhuin@unifiedinfotech.net>
2024-11-19 23:15:05 +0530

    #

--- database/migrations/2024_11_19_174419_add_fields_to_users_table.php
+++ database/migrations/2024_11_19_174419_add_fields_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropUnique(['email']); 
+            $table->dropUnique(['mobile_no']);
         });
     }
 
@@ -22,8 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unique('email');
-           
+            $table->unique('mobile_no');
         });
     }
 };
